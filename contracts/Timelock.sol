b571ac57148e388d8f5d6f4309b122fb1743a9f6
xoxomaster <xoxofinancemaster@gmail.com>
2021-02-18 09:40:25 +0100

    Remove comments

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -8,10 +8,8 @@
 //
 // Ctrl+f for XXX to see all the modifications.
 
-// XXX: pragma solidity ^0.5.16;
-pragma solidity ^0.6.2;
+pragma solidity 0.6.2;
 
-// XXX: import "./SafeMath.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Timelock {
